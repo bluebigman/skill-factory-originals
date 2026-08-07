d691e63459dca85c07407b1f7b5dd67ee4259c02
bluebigman <bluebigman@users.noreply.github.com>
2026-08-08 02:44:25 +0800

    原创资产提交

--- browser-use/scripts/main.py
+++ browser-use/scripts/main.py
@@ -11,7 +11,7 @@ import argparse
 import json
 import re
 import sys
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 
 # ---------------------------------------------------------------------------
